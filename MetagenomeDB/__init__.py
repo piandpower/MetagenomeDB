baeda711880ac7a5425a670e11fd4d244f3b641b
Aurelien Mazurie <ajmazurie@oenone.net>
2010-04-13 14:58:23 -0600

    Deleted export.Cytoscape (will be provided as an example in the documentation)

--- MetagenomeDB/__init__.py
+++ MetagenomeDB/__init__.py
@@ -1,4 +1,5 @@
 
 from commons import set_debug_level
 from objects import Collection, Sequence, Relationship
-from mapping import UncommittedObject, DuplicateObject
+from mapping import UncommittedObject
+from forge import DuplicateObject
