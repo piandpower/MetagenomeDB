340ff2aaded6dd348aaba79e33252eeae01c4718
Aurelien Mazurie <ajmazurie@oenone.net>
2011-06-28 12:30:18 -0600

    mdb-import-BLAST-alignments: Fixed a harmless bug that would display an error message after the BLAST is imported

--- lib/MetagenomeDB/__init__.py
+++ lib/MetagenomeDB/__init__.py
@@ -1,8 +1,8 @@
 
 __version_major__ = 0
 __version_minor__ = 2
-__revision__ = 10
-__build__ = "63913D1"
+__revision__ = 11
+__build__ = "3349BC1"
 
 version = "%s.%s (revision %s, build %s)" % (
 	__version_major__,
